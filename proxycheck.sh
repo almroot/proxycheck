cf326a7a87e73747aa8365209ae64fc74df4199f
Fredrik N. Almroth <fredrik@detectify.com>
2022-11-14 14:02:21 +0100

    Fix broken sources, add more sources

--- proxycheck.sh
+++ proxycheck.sh
@@ -7,30 +7,56 @@ benchmark1="http://example.com/"
 benchmark2="https://example.com/"
 
 # callbacks where each must return a list of IP:port pairs
-list1 () { curl 'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all' --silent; }
-list2 () { curl 'https://proxylist.geonode.com/api/proxy-list?limit=1000&page=1&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps' --silent | jq -c '.data[] | "\(.ip):\(.port)"' | cut '-d"' -f2; }
-list3 () { curl 'https://proxylist.geonode.com/api/proxy-list?limit=1000&page=2&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps' --silent | jq -c '.data[] | "\(.ip):\(.port)"' | cut '-d"' -f2; }
-list4 () { curl 'https://proxylist.geonode.com/api/proxy-list?limit=1000&page=3&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps' --silent | jq -c '.data[] | "\(.ip):\(.port)"' | cut '-d"' -f2; }
-list5 () { curl 'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt' --silent; }
-list6 () { curl 'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt' --silent; }
-list7 () { curl 'https://github.com/ShiftyTR/Proxy-List/blob/master/http.txt' --silent; }
-list8 () { curl 'https://github.com/ShiftyTR/Proxy-List/blob/master/https.txt' --silent; }
-list9 () { curl 'https://github.com/monosans/proxy-list/blob/main/proxies_anonymous/http.txt' --silent; }
-list10 () { curl 'https://multiproxy.org/txt_all/proxy.txt' --silent; }
+list01 () { curl 'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all' --silent; }
+list02 () { curl 'https://github.com/jetkai/proxy-list/blob/main/archive/txt/proxies-http.txt' --silent; }
+list03 () { curl 'https://github.com/jetkai/proxy-list/blob/main/archive/txt/proxies-https.txt' --silent; }
+list04 () { curl 'https://github.com/jetkai/proxy-list/blob/main/online-proxies/txt/proxies-http.txt' --silent; }
+list05 () { curl 'https://github.com/jetkai/proxy-list/blob/main/online-proxies/txt/proxies-https.txt' --silent; }
+list06 () { curl 'https://multiproxy.org/txt_all/proxy.txt' --silent; }
+list07 () { curl 'https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps' --silent | jq -c '.data[] | "\(.ip):\(.port)"' | cut '-d"' -f2; }
+list08 () { curl 'https://proxylist.geonode.com/api/proxy-list?limit=500&page=2&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps' --silent | jq -c '.data[] | "\(.ip):\(.port)"' | cut '-d"' -f2; }
+list09 () { curl 'https://proxylist.geonode.com/api/proxy-list?limit=500&page=3&sort_by=lastChecked&sort_type=desc&protocols=http%2Chttps' --silent | jq -c '.data[] | "\(.ip):\(.port)"' | cut '-d"' -f2; }
+list10 () { curl 'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt' --silent; }
+list11 () { curl 'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt' --silent; }
+list12 () { curl 'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt' --silent; }
+list13 () { curl 'https://raw.githubusercontent.com/almroot/proxylist/master/list.txt' --silent; }
+list14 () { curl 'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt' --silent; }
+list15 () { curl 'https://raw.githubusercontent.com/hendrikbgr/Free-Proxy-Repo/master/proxy_list.txt' --silent; }
+list16 () { curl 'https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt' --silent; }
+list17 () { curl 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt' --silent; }
+list18 () { curl 'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_anonymous/http.txt' --silent; }
+list19 () { curl 'https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http.txt' --silent; }
+list20 () { curl 'https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http_old.txt' --silent; }
+list21 () { curl 'https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt' --silent; }
+list22 () { curl 'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt' --silent; }
+list23 () { curl 'https://raw.githubusercontent.com/zeynoxwashere/proxy-list/main/http.txt' --silent; }
 
 # fetch proxy server candidates
 rm "$corpusfile" 2>/dev/null
 {
-  list1;
-  list2;
-  list3;
-  list4;
-  list5;
-  list6;
-  list7;
-  list8;
-  list9;
+  list01;
+  list02;
+  list03;
+  list04;
+  list05;
+  list06;
+  list07;
+  list08;
+  list09;
   list10;
+  list11;
+  list12;
+  list13;
+  list14;
+  list15;
+  list16;
+  list17;
+  list18;
+  list19;
+  list20;
+  list21;
+  list22;
+  list23;
 } > "$corpusfile"
 
 # we need to know how some web servers respond to eliminate bad (malicious) servers
